f2fde298894e8b68d16c98436d4b956dbe1f7a5c
LetusCasa13 <lgonzalez@elpuig.xeill.net>
2025-09-21 10:54:11 +0200

    Mostrar menu por pantalla

---
+++ src/MostrarMenuPantalla.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MostrarMenuPantalla {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Wellcome to the agenda, please choose an option: ");
+        System.out.println("1. Create a new contact. ");
+        System.out.println("2. Search for existing contacts.");
+        System.out.println("3. Update an existing contact. ");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exist.");
+
+
+
+
+    }
+
+}
